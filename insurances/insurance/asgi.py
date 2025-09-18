1230d2a91d7f2021276365d27c8fee16bf859c06
saran <saranvms2002@gmail.com>
2025-09-18 19:57:35 +0530

    day5

--- insurances/insurance/asgi.py
+++ insurances/insurance/asgi.py
@@ -11,7 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'insurance.settings')
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'insurance.insurance.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'insurance.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'insurance.insurance.settings')
 application = get_asgi_application()
--- insurances/insurance/settings.py
+++ insurances/insurance/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'insuranceapp', 
-    'insurance.insuranceapp',
+    'insuranceapp',
 ]
 
 MIDDLEWARE = [
